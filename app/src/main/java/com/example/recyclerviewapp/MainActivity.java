81c9d2856645ad1688fc48b5d1a13de1c8cb136f
divyapannala <divyapannala07@gmail.com>
2021-06-28 11:12:36 +0530

    Final code

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.java
@@ -61,46 +61,50 @@ recyclerview.setAdapter(adaptery);
         displaybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String eid=pemail.getText().toString();
-                String econtact=pcontact.getText().toString();
+                String eid = pemail.getText().toString().trim();
+                String econtact = pcontact.getText().toString().trim();
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
-                if(eid.isEmpty() && econtact.isEmpty()){
-                    Toast.makeText(MainActivity.this,"Enter The details",Toast.LENGTH_SHORT).show();
+                if (eid.isEmpty() && econtact.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Enter The details", Toast.LENGTH_SHORT).show();
                 }
+                    else {
+                    if (eid.matches(emailPattern) && econtact.length()==10) {
+                        Toast.makeText(getApplicationContext(), "Enter valid email and contact", Toast.LENGTH_SHORT).show();
+                        try {
 
-                else{
-                    try {
-
-                        boolean  userExist = false;
-                        for (PersonDetails pern : detailsList) {
-                            if (pern.getPersonemail().equalsIgnoreCase(eid) || pern .getPersoncontact().equalsIgnoreCase(econtact) ) {
-                                //user exist
-                                userExist = true;
+                            boolean userExist = false;
+                            for (PersonDetails pern : detailsList) {
+                                if (pern.getPersonemail().equalsIgnoreCase(eid) || pern.getPersoncontact().equalsIgnoreCase(econtact)) {
+                                    //user exist
+                                    userExist = true;
+                                }
                             }
-                        }
 
-                        if(userExist)
-                        {
-                            Toast.makeText(MainActivity.this,"Already user exits",Toast.LENGTH_SHORT).show();
-                        }
-                        else
-                        {
-                            detailsList.add(new PersonDetails(eid, econtact));
-                            adaptery.notifyItemInserted(detailsList.size() - 1);
+                            if (userExist) {
+                                Toast.makeText(MainActivity.this, "Already user exits", Toast.LENGTH_SHORT).show();
+                            } else {
+                                detailsList.add(new PersonDetails(eid, econtact));
+                                adaptery.notifyItemInserted(detailsList.size() - 1);
 
-                            e.putString("usersList",  new Gson().toJson(detailsList));
-                            e.commit();//---saves the values---
-                            Toast.makeText(MainActivity.this, "User Details Registered",Toast.LENGTH_SHORT).show();
-                            pemail.setText("");
-                            pcontact.setText("");
-                        }
+                                e.putString("usersList", new Gson().toJson(detailsList));
+                                e.commit();//---saves the values---
+                                Toast.makeText(MainActivity.this, "User Details Registered", Toast.LENGTH_SHORT).show();
+                                pemail.setText("");
+                                pcontact.setText("");
+                            }
 
-                    }catch (NumberFormatException exception){
-                        Toast.makeText(MainActivity.this,"Cannot Display",Toast.LENGTH_SHORT).show();
+                        } catch (NumberFormatException exception) {
+                            Toast.makeText(MainActivity.this, "Cannot Display", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Invalid email address", Toast.LENGTH_SHORT).show();
                     }
-                }
 
-            }
+
+                }}
+
+
         });
     }
 
